95b72d1d4e1a1c227e2739acbc6d614069f8ff84
Utkarsh <famedustevershine@gmail.com>
2023-04-11 00:13:39 +0530

    f

--- src/index.js
+++ src/index.js
@@ -19,60 +19,60 @@ const corsOptions ={
 
 app.use(cors())
 
-const io = require("socket.io")(8900, {
-    cors: {
-      origin: "https://chat-app-nu-tawny.vercel.app/",
-    },
-});
+// const io = require("socket.io")(8900, {
+//     cors: {
+//       origin: "https://chat-app-nu-tawny.vercel.app/",
+//     },
+// });
 
 
-let users = [];
+// let users = [];
 
 
-const addUser = (userId, socketId) => {
-    if(userId){
-        !users.some((user) => user.userId === userId) &&
-        users.push({ userId, socketId });
-    }
+// const addUser = (userId, socketId) => {
+//     if(userId){
+//         !users.some((user) => user.userId === userId) &&
+//         users.push({ userId, socketId });
+//     }
 
-};
+// };
 
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
+// const removeUser = (socketId) => {
+//   users = users.filter((user) => user.socketId !== socketId);
+// };
 
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId);
-};
+// const getUser = (userId) => {
+//   return users.find((user) => user.userId === userId);
+// };
 
 
 
-io.on("connection", (socket)=>{
-    console.log("a user connected.");
+// io.on("connection", (socket)=>{
+//     console.log("a user connected.");
 
-    //take userId and socketId from user
-    socket.on("addUser", (userId) => {
-      addUser(userId, socket.id);
-      io.emit("getUsers", users);
-    });
+//     //take userId and socketId from user
+//     socket.on("addUser", (userId) => {
+//       addUser(userId, socket.id);
+//       io.emit("getUsers", users);
+//     });
   
-    //send and get message
-    socket.on("sendMessage", ({ senderId, receiverId, text, conversationId }) => {
-        const user = getUser(receiverId);
-        io.to(user.socketId).emit("getMessage", {
-            conversationId,
-            senderId,
-            text,
-      });
-    });
+//     //send and get message
+//     socket.on("sendMessage", ({ senderId, receiverId, text, conversationId }) => {
+//         const user = getUser(receiverId);
+//         io.to(user.socketId).emit("getMessage", {
+//             conversationId,
+//             senderId,
+//             text,
+//       });
+//     });
   
-    //when disconnect
-    socket.on("disconnect", () => {
-      console.log("a user disconnected!");
-      removeUser(socket.id);
-      io.emit("getUsers", users);
-    });
-})
+//     //when disconnect
+//     socket.on("disconnect", () => {
+//       console.log("a user disconnected!");
+//       removeUser(socket.id);
+//       io.emit("getUsers", users);
+//     });
+// })
 
 
 
