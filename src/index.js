e676d5679bd490c3dbf39bd18877f76690711835
Utkarsh <famedustevershine@gmail.com>
2023-05-13 23:48:06 +0530

    .

--- src/index.js
+++ src/index.js
@@ -22,8 +22,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = socketIO(server,{
     cors: {
-      origin: "https://chat-app-nu-tawny.vercel.app",
-      // origin:"http:localhost:3000",
+      origin: ["https://chat-app-nu-tawny.vercel.app","http:localhost:3000"],
       credentials:true,    
       optionSuccessStatus:200,
     },
