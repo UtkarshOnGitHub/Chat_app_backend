9ced8f2e7cd3d70c275418494e972b98d9696341
Utkarsh <famedustevershine@gmail.com>
2023-05-17 22:09:24 +0530

    .

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const corsOptions ={
     credentials:true,    
     optionSuccessStatus:200
 }
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 
