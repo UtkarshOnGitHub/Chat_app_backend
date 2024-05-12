3a26b0c330fe3993628e415addddfe116d2c1015
Utkarsh <famedustevershine@gmail.com>
2024-05-12 13:26:03 +0530

    new db password updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 const dbConnect = ()=>{
     mongoose.set('strictQuery', true);
-    return mongoose.connect('mongodb+srv://kira:JQocuD5RyJE5AU5l@cluster0.vtcz0fs.mongodb.net/?retryWrites=true&w=majority');
+    return mongoose.connect(process.env.DB_URL).then((res)=>{
+        console.log("Database Connected Successfully!")
+    }).catch((err)=>{
+        console.log(err)
+    });
 }
 module.exports = dbConnect;
